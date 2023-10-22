7b8a120cb6f5180f9425c72bc27723c640277b4c
viswa2497 <148688023+viswa2497@users.noreply.github.com>
2023-10-22 17:03:54 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.little('my paresnts new healthy diner')
+
+streamlit.header('breakfast menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale,spinach &rocket smoothiee')
+streamlit.text('hard-boiled free-range egg')
