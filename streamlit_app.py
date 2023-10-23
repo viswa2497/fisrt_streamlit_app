81b3801d076b8e7c5999ec2473c203a59a53648a
viswa2497 <148688023+viswa2497@users.noreply.github.com>
2023-10-23 18:07:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 avacodo toast')
+
+
+import snowfalke.connector
